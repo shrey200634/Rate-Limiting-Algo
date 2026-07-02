51510229bd3c83fae948bf7191e81dc284cc7e10
shrey200634 <dshrey60@gmail.com>
2026-07-02 15:14:47 +0530

    make structure of fixed window

---
+++ fixed-window/src/main/java/com/ratelimiter/fixed_window/FixedWindowApplication.java
@@ -0,0 +1,13 @@
+package com.ratelimiter.fixed_window;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixedWindowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixedWindowApplication.class, args);
+	}
+
+}
---
+++ fixed-window/src/test/java/com/ratelimiter/fixed_window/FixedWindowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratelimiter.fixed_window;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixedWindowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
