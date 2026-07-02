edb282bebd679737046b221f5c0003aff2f54104
shrey200634 <dshrey60@gmail.com>
2026-07-03 00:45:02 +0530

    add docker compose for the redis - compleye service of the fixed window using lua script

--- fixed-window/src/main/java/com/ratelimiter/fixed_window/service/FixedWindowRateLimiterService.java
+++ fixed-window/src/main/java/com/ratelimiter/fixed_window/service/FixedWindowRateLimiterService.java
@@ -1,11 +1,14 @@
 package com.ratelimiter.fixed_window.service;
 
+import com.ratelimiter.common.dto.RateLimitResult;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.util.Collections;
 import java.util.List;
 @AllArgsConstructor
 @Service
@@ -19,6 +22,28 @@ public class FixedWindowRateLimiterService {
 
     @Value("${ratelimit.fixed-window.window-seconds:60}")
     private int windowSeconds ;
+
+    public RateLimitResult tryAcquire(String identifier ){
+        String key = "ratelimit:fixed:" + identifier;
+
+        @SuppressWarnings("unchecked")
+                List<Long> res = redisTemplate.execute(
+                        fixedWindowScript,
+                Collections.singletonList(key),
+                String.valueOf(limit),
+                String.valueOf(windowSeconds)
+        );
+
+        long allowedFlag = res.get(0);
+        long currentCount = res.get(1);
+        long ttl = res.get(2);
+
+        boolean allowd = allowedFlag==1 ;
+        long remaining = Math.max(0,limit-currentCount);
+        long resetAt = Instant.now().getEpochSecond() + ttl;
+
+        return new RateLimitResult(allowd , remaining, resetAt);
+    }
 
 
 }
